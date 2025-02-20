0b492e95e40784b2667905f40bc8e1b80fe1b6de
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-02-20 11:42:34 +0100

    array methods part 2

--- SystemManager.playground/Contents.swift
+++ SystemManager.playground/Contents.swift
@@ -1,11 +1,23 @@
 let math = Subject(name: "Math", teacherName: "Mr. Smith", level: SubjectLevel.intermediate)
 let spanish = Subject(name: "Spanish", teacherName: "Elena Montiel", level: .basic)
+let biology = Subject(name: "Biology", teacherName: "Mr. Parker", level: .intermediate)
+let economy = Subject(name: "Economy", teacherName: "Mr. Morales", level: .intermediate)
 
-let s1 = Student(name: "Vegeta", age: 29, email: "vegetassj1@gmail.com", subjects: [math, spanish], scores: [70, 80]);
-let s2 = AdvancedStudent(name: "Goku", age: 29, email: "gokussj2@gmail.com", subjects: [math, spanish], scores: [90, 75], extraPoints: 10)
+let s1 = Student(name: "Vegeta", age: 29, email: "vegetassj1@gmail.com", subjects: [], scores: []);
+let s2 = AdvancedStudent(name: "Goku", age: 29, email: "gokussj2@gmail.com", subjects: [], scores: [], extraPoints: 10)
 
-let desS1 = s1.describe()
-let descS2 = s2.describe()
+let service = StudentsManager(students: [])
+service.insertStudent(s1)
+service.insertStudent(s2)
 
-print(desS1)
-print(descS2)
+service.asignSubjectToStudent(subject: math, score: 80, student: s1)
+service.asignSubjectToStudent(subject: spanish, score: 70, student: s1)
+service.asignSubjectToStudent(subject: biology, score: 90, student: s1)
+
+/*
+service.asignSubjectToStudent(subject: math, score: 80, student: s2)
+service.asignSubjectToStudent(subject: spanish, score: 96, student: s2)
+service.asignSubjectToStudent(subject: economy, score: 100, student: s2)
+*/
+
+service.generateStudentsReport()
